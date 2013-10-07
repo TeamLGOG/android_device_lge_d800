f2dba6a56b52c1aad5c1bb53f7cb1578e71d6b18
Seth Shelnutt <shelnutt2@gmail.com>
2013-10-07 14:21:04 -0400

    Add DT target for new custombootimg.mk
    Change-Id: I94c1f1b9d44e927d61e9da69019eb62ab3c8d8f9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@
 #
 
 TARGET_KERNEL_CONFIG := cyanogenmod_g2-att-perf_defconfig
-
+G2_DTS_TARGET := msm8974-g2-att
 
 # inherit from d800-common
 -include device/lge/d800-common/BoardConfigCommon.mk
